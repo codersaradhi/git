10548ad50859ea095e891b44384a1b5c5362af2b
codersaradhi <chaitanyasaradhi1999@gmail.com>
2024-04-04 11:29:18 +0530

    jsmodules are done

---
+++ jsbasics/scoping/main_script.js
@@ -0,0 +1,14 @@
+// const usergreetings = require ('./message.js');
+
+// // console.log(usergreetings);
+
+// console.log(usergreetings.message());
+
+// for html page///////
+import { name, age } from "./person.js";
+import message from "./message.js";
+
+let text = "My name is " + name + ", I am " + age + ".";
+
+document.getElementById("demo_person").innerHTML = text;
+document.getElementById("demo_message").innerHTML = message();
---
+++ jsbasics/scoping/message.js
@@ -0,0 +1,20 @@
+const message = () => {
+    const name = "chaitu";
+    const age = 24;
+    return name + ' is ' + age + ' years old.';
+    };
+// message();
+
+// function message() {
+//     // console.log("hello chaitu");
+//     const name = "chaitu";
+//     const age = 24;
+//     // return name + ' is ' + age + ' years old.';
+//     console.log(name + ' is ' + age + ' years old.');
+// };
+// message();
+    
+export default message;
+// module.exports = {
+//     message
+// }
